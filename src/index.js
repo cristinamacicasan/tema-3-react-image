00e4b0329c5df5646f2b41cdf9f76b81c1370e08
cristinamacicasan <cmacicasan@gmail.com>
2024-04-13 12:11:26 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import ReactDOM from 'react-dom';
+import App from './components/App'; // Folosește importul corect
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
