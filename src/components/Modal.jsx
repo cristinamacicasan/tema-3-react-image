fd6b225026ce939ac5f609675ded033fe478b97d
cristinamacicasan <cmacicasan@gmail.com>
2024-04-13 13:29:12 +0300

    add styles on modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,25 +1,29 @@
-// Modal.jsx
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Modal.module.css'; // Importăm stilurile pentru Modal
+import styles from './Modal.module.css'; 
 
 const Modal = ({ isOpen, imageUrl, onClose }) => {
-  if (!isOpen) return null; // Returnăm null dacă modalul nu este deschis
+  const handleCloseModal = (event) => {
+    if (event.target === event.currentTarget) {
+      onClose(); 
+    }
+  };
+
+  if (!isOpen) return null; 
 
   return (
-    <div className={styles.overlay} onClick={onClose}>
+    <div className={styles.overlay} onClick={handleCloseModal}>
       <div className={styles.modal}>
         <img src={imageUrl} alt="Large" />
-        <button className={styles.closeButton} onClick={onClose}>Close</button>
       </div>
     </div>
   );
 };
 
 Modal.propTypes = {
-  isOpen: PropTypes.bool.isRequired, // indicatorul pentru deschiderea sau închiderea modalului
-  imageUrl: PropTypes.string.isRequired, // adresa URL a imaginii mari
-  onClose: PropTypes.func.isRequired, // funcția de închidere a modalului
+  isOpen: PropTypes.bool.isRequired, 
+  imageUrl: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired, 
 };
 
 export default Modal;
